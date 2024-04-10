bf60fbd7804dc7ec3ee54d8a70137633b467c56b
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-10 20:47:12 +0800

    Update Homies.lua

--- chicken/Homies.lua
+++ chicken/Homies.lua
@@ -1,3 +1,38 @@
 if game.PlaceId == 16190471004 then
-    print("Hi")
+    local ReplicatedStorage = game:GetService("ReplicatedStorage")
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    local PlayerGui = LocalPlayer.PlayerGui
+
+    local MainData = LocalPlayer:WaitForChild("MAIN_DATA")
+    local CurrentData = MainData:WaitForChild("Fruits"):WaitForChild(MainData:WaitForChild("Slots")[MainData:WaitForChild("Slot").Value].Value)
+
+    local FruitMoves = {}
+
+    while true do
+        wait(0.25)
+
+        if PlayerGui:FindFirstChild("UI") and not PlayerGui.UI.HUD.Visible then
+            return
+        end
+        if LocalPlayer.Character.Stats:GetAttribute("Stamina") <= 50 then
+            return
+        end
+
+        if #FruitMoves == 0 then
+            for i,v in pairs(LocalPlayer.Backpack:GetChildren()) do
+                if v.ClassName == "Tool" and CurrentData.Level.Value >= v:GetAttribute("Level") then
+                    FruitMoves[#FruitMoves + 1] = string.gsub(v.Name, " ", "")
+                end
+            end
+        else
+            LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(122, 149, -1264)
+
+            for i,v in pairs(FruitMoves) do
+                if not LocalPlayer.Cooldowns:FindFirstChild(v) then
+                    ReplicatedStorage.Replicator:InvokeServer(CurrentData.Name, v, {})
+                end
+            end
+        end
+    end
 end
