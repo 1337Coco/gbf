429d1afb7b3bf39d8ad930f864356cd6c16ecdc4
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-10 20:41:59 +0800

    Update loader.lua

--- chicken/loader.lua
+++ chicken/loader.lua
@@ -3,7 +3,7 @@ local PlaceIDs = {
     [16190471004] = "Homies"
 }
 
-if placeIDs[game.PlaceId] then
+if PlaceIDs[game.PlaceId] then
     local scriptURL = "https://raw.githubusercontent.com/1337Coco/gbf/main/chicken/" .. PlaceIDs[game.PlaceId] .. ".lua"
     local scriptContent = game:HttpGet(scriptURL)
     loadstring(scriptContent)()
