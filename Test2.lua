94d97ff3e9a9c89bae3fce2bc6a44a5d38b6cd4b
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-14 20:22:24 +0800

    Update Test2.lua

--- Test2.lua
+++ Test2.lua
@@ -4,7 +4,7 @@ if game.PlaceId == 9224601490 then
     local LocalPlayer = Players.LocalPlayer
     local StarterGui = game:GetService("StarterGui")
     local Workspace = game:GetService("Workspace")
-    
+    local Backpack = LocalPlayer.Backpack
     local MainData = LocalPlayer:WaitForChild("MAIN_DATA")
     local CurrentData = MainData:WaitForChild("Fruits"):WaitForChild(MainData:WaitForChild("Slots")[MainData:WaitForChild("Slot").Value].Value)
 
