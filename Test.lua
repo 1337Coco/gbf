e30d76f313cbb7c296ccfc122d1037d618e1e344
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-14 08:19:10 +0800

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -79,4 +79,21 @@ while true do
     CheckPlayerStatus()
     -- Check if the player is in the specified position and perform fruit moves if so
     CheckPlayerPosition()
+
+    -- Perform fruit moves if FruitMoves is empty
+    if #FruitMoves == 0 then
+        for i, v in pairs(LocalPlayer.Backpack:GetChildren()) do
+            if v.ClassName == "Tool" and CurrentData.Level.Value >= v:GetAttribute("Level") then
+                FruitMoves[#FruitMoves + 1] = string.gsub(v.Name, " ", "")
+            end
+        end
+    else
+        LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(122, 149, -1264)
+
+        for i, v in pairs(FruitMoves) do
+            if not LocalPlayer.Cooldowns:FindFirstChild(v) then
+                ReplicatedStorage.Replicator:InvokeServer(CurrentData.Name, v, {})
+            end
+        end
+    end
 end
