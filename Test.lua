9c3566f52c87241a7e3f91891d86101b9f2c4cc2
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-14 08:28:22 +0800

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,72 +1,48 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-local StarterGui = game:GetService("StarterGui")
-local Workspace = game:GetService("Workspace")
+if game.PlaceId == 12413901502 then
+    local ReplicatedStorage = game:GetService("ReplicatedStorage")
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    local PlayerGui = LocalPlayer.PlayerGui
+	local character = LocalPlayer.Character
 
--- Function to respawn the player
-local Respawn = function()
-    require(ReplicatedStorage.Loader).ServerEvent("Core", "LoadCharacter", {})
-    require(ReplicatedStorage.Loader).ServerEvent("Main", "LoadCharacter")
-    if (LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()) and LocalPlayer.Character:WaitForChild("Humanoid", 10) then
-        Workspace.CurrentCamera.CameraSubject = LocalPlayer.Character
-        StarterGui:SetCoreGuiEnabled("Backpack", false)
-        StarterGui:SetCoreGuiEnabled("PlayerList", false)
-        StarterGui:SetCoreGuiEnabled("Chat", false)
-    end
-end
+    local MainData = LocalPlayer:WaitForChild("MAIN_DATA")
+    local CurrentData = MainData:WaitForChild("Fruits"):WaitForChild(MainData:WaitForChild("Slots")[MainData:WaitForChild("Slot").Value].Value)
 
--- Function to get the equipped fruit
-local function GetFruit()
-    local MainData = LocalPlayer.MAIN_DATA
-    local CurrentData = MainData.Fruits:WaitForChild(MainData.Slots[MainData.Slot.Value].Value)
-    return CurrentData.Name
-end
+    local FruitMoves = {}
 
--- Function to check if the player is dead and respawn if necessary
-local function CheckPlayerStatus()
-    local character = LocalPlayer.Character
-    local UI = LocalPlayer.PlayerGui.UI
+    while true do
+        wait(0.25)
 
-    if (not character or character == nil) and not UI.HUD.Visible then
-        Respawn()  -- Respawn the player if dead
-    end
-end
-
--- Function to check if the player is in the specified position and perform fruit moves if so
-local function CheckPlayerPosition()
-    local character = LocalPlayer.Character
-
-    if character and character:FindFirstChild("HumanoidRootPart") then
+        require(ReplicatedStorage.Loader).ServerEvent("Core", "LoadCharacter", {})
+		require(ReplicatedStorage.Loader).ServerEvent("Main", "LoadCharacter")
+			if (LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()) and LocalPlayer.Character:WaitForChild("Humanoid", 10) then
+				Workspace.CurrentCamera.CameraSubject = LocalPlayer.Character
+				StarterGui:SetCoreGuiEnabled("Backpack", false)
+				StarterGui:SetCoreGuiEnabled("PlayerList", false)
+				StarterGui:SetCoreGuiEnabled("Chat", false)
+			end
+			
+		if character and character:FindFirstChild("HumanoidRootPart") then
         -- Move the player to the specified position if not already there
-        if character.HumanoidRootPart.Position ~= Vector3.new(-4773, 1349, -279) then
-            character.HumanoidRootPart.CFrame = CFrame.new(-4773, 1349, -279)
-        else
-            wait()
-        end
-    end
-end
+			if character.HumanoidRootPart.Position ~= Vector3.new(-4773, 1349, -279) then
+				character.HumanoidRootPart.CFrame = CFrame.new(-4773, 1349, -279)
+			else
+				if #FruitMoves == 0 then
+					for i,v in pairs(LocalPlayer.Backpack:GetChildren()) do
+						if v.ClassName == "Tool" and CurrentData.Level.Value >= v:GetAttribute("Level") then
+                    FruitMoves[#FruitMoves + 1] = string.gsub(v.Name, " ", "")
+						end
+					end
+				else
+            
+				for i,v in pairs(FruitMoves) do
+					if not LocalPlayer.Cooldowns:FindFirstChild(v) then
+						ReplicatedStorage.Replicator:InvokeServer(CurrentData.Name, v, {})
+					end
+				end
+				end
+			end
+		end
 
--- Continuously check player status and position
-while true do
-    wait(0.25)
-    -- Check if the player is dead and respawn if necessary
-    CheckPlayerStatus()
-    -- Check if the player is in the specified position and perform fruit moves if so
-    CheckPlayerPosition()
-    if #FruitMoves == 0 then
-        for i, v in pairs(LocalPlayer.Backpack:GetChildren()) do
-            if v.ClassName == "Tool" and CurrentData.Level.Value >= v:GetAttribute("Level") then
-                FruitMoves[#FruitMoves + 1] = string.gsub(v.Name, " ", "")
-            end
-        end
-    else
-        LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(122, 149, -1264)
-
-        for i, v in pairs(FruitMoves) do
-            if not LocalPlayer.Cooldowns:FindFirstChild(v) then
-                ReplicatedStorage.Replicator:InvokeServer(CurrentData.Name, v, {})
-            end
-        end
-    end
+    end --wow
 end
