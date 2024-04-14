655731475a42dab27843ad1f3e448bbc207fa1da
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-14 09:15:29 +0800

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -5,15 +5,10 @@ if game.PlaceId == 12413901502 then
     local PlayerGui = LocalPlayer.PlayerGui
     local StarterGui = game:GetService("StarterGui")
     local Workspace = game:GetService("Workspace")
-
-
     local MainData = LocalPlayer:WaitForChild("MAIN_DATA")
     local CurrentData = MainData:WaitForChild("Fruits"):WaitForChild(MainData:WaitForChild("Slots")[MainData:WaitForChild("Slot").Value].Value)
-	
-    
-    local FruitMoves = {}
-	
-	local Respawn = function()
+
+    local Respawn = function()
     require(ReplicatedStorage.Loader).ServerEvent("Core", "LoadCharacter", {})
     require(ReplicatedStorage.Loader).ServerEvent("Main", "LoadCharacter")
     if (LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()) and LocalPlayer.Character:WaitForChild("Humanoid", 10) then
@@ -23,16 +18,33 @@ if game.PlaceId == 12413901502 then
         StarterGui:SetCoreGuiEnabled("Chat", false)
     end
 end
+    
+    local FruitMoves = {}
+	
+     local Respawn = function()
+         require(ReplicatedStorage.Loader).ServerEvent("Core", "LoadCharacter", {})
+         require(ReplicatedStorage.Loader).ServerEvent("Main", "LoadCharacter")
+         if (LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()) and LocalPlayer.Character:WaitForChild("Humanoid", 10) then
+           Workspace.CurrentCamera.CameraSubject = LocalPlayer.Character
+           StarterGui:SetCoreGuiEnabled("Backpack", false)
+           StarterGui:SetCoreGuiEnabled("PlayerList", false)
+           StarterGui:SetCoreGuiEnabled("Chat", false)
+         end
+end
+	
+     local function CheckPlayerStatus()
+         local character = LocalPlayer.Character
+         local UI = LocalPlayer.PlayerGui.UI
 
+         if (not character or character == nil) and not UI.HUD.Visible then
+           Respawn()  -- Respawn the player if dead
+         end
+end
     while true do
         wait(0.1)
-	local character = LocalPlayer.Character
-    	local UI = LocalPlayer.PlayerGui.UI
-	if (not character or character == nil) and not UI.HUD.Visible then
-		Respawn()
-	end
+	CheckPlayerStatus()
 
-
+	
         if #FruitMoves == 0 then
             for i,v in pairs(LocalPlayer.Backpack:GetChildren()) do
                 if v.ClassName == "Tool" and CurrentData.Level.Value >= v:GetAttribute("Level") then
