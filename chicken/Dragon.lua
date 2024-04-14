6948003ef1c4c026c3dfff5925ce7e4d84ec32de
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-14 09:29:52 +0800

    Update Dragon.lua

--- chicken/Dragon.lua
+++ chicken/Dragon.lua
@@ -2,47 +2,21 @@ if game.PlaceId == 12413901502 then
     local ReplicatedStorage = game:GetService("ReplicatedStorage")
     local Players = game:GetService("Players")
     local LocalPlayer = Players.LocalPlayer
-    local StarterGui = game:GetService("StarterGui")
-    local Workspace = game:GetService("Workspace")
+    local PlayerGui = LocalPlayer.PlayerGui
 
-    -- Function to respawn the player
-    local Respawn = function()
-        require(ReplicatedStorage.Loader).ServerEvent("Core", "LoadCharacter", {})
-        require(ReplicatedStorage.Loader).ServerEvent("Main", "LoadCharacter")
-        if (LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()) and LocalPlayer.Character:WaitForChild("Humanoid", 10) then
-            Workspace.CurrentCamera.CameraSubject = LocalPlayer.Character
-            StarterGui:SetCoreGuiEnabled("Backpack", false)
-            StarterGui:SetCoreGuiEnabled("PlayerList", false)
-            StarterGui:SetCoreGuiEnabled("Chat", false)
-        end
-    end
+    local MainData = LocalPlayer:WaitForChild("MAIN_DATA")
+    local CurrentData = MainData:WaitForChild("Fruits"):WaitForChild(MainData:WaitForChild("Slots")[MainData:WaitForChild("Slot").Value].Value)
 
-    -- Function to get the equipped fruit
-    local function GetFruit()
-        local MainData = LocalPlayer.MAIN_DATA
-        local CurrentData = MainData.Fruits:WaitForChild(MainData.Slots[MainData.Slot.Value].Value)
-        return CurrentData.Name
-    end
+    local FruitMoves = {}
 
-    -- Function to check if the player is dead and respawn if necessary
-    local function CheckPlayerStatus()
-        local character = LocalPlayer.Character
-        local UI = LocalPlayer.PlayerGui.UI
-
-        if (not character or character == nil) and not UI.HUD.Visible then
-            Respawn()  -- Respawn the player if dead
-        end
-    end
-
-    -- Continuously check player status and position
     while true do
         wait(0.25)
-        -- Check if the player is dead and respawn if necessary
-        CheckPlayerStatus()
-        if character.HumanoidRootPart.Position ~= Vector3.new(-4773, 1349, -279) then
-            character.HumanoidRootPart.CFrame = CFrame.new(-4773, 1349, -279)
-        else
-            wait()
+
+        if PlayerGui:FindFirstChild("UI") and not PlayerGui.UI.HUD.Visible then
+            return
+        end
+        if LocalPlayer.Character.Stats:GetAttribute("Stamina") <= 50 then
+            return
         end
 
         if #FruitMoves == 0 then
