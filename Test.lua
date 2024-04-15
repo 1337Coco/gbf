7bd9c347967b645c5468249e56e54e678aaa4aad
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-16 04:02:25 +0800

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,26 +1,22 @@
 local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 local RESPAWN_DELAY = 5
 
 Players.CharacterAutoLoads = false
 
-local function onPlayerAdded(player)
-    local function onCharacterAdded(character)
-        local humanoid = character:WaitForChild("Humanoid")
+local function onCharacterAdded(character)
+    local humanoid = character:WaitForChild("Humanoid")
 
-        local function onDied()
-            task.wait(RESPAWN_DELAY)
-            if player.Character == nil then
-                print("Respawning player:", player.Name)
-                player:LoadCharacter()
-            end
+    local function onDied()
+        task.wait(RESPAWN_DELAY)
+        if LocalPlayer.Character == nil then
+            print("Respawning local player...")
+            LocalPlayer:LoadCharacter()
         end
-
-        humanoid.Died:Connect(onDied)
     end
 
-    player.CharacterAdded:Connect(onCharacterAdded)
-
-    player:LoadCharacter()
+    humanoid.Died:Connect(onDied)
 end
 
-Players.PlayerAdded:Connect(onPlayerAdded)
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
+LocalPlayer:LoadCharacter()
