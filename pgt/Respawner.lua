87dddf4130a6899ecc08bc500ee58f8ff5a46718
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-17 06:19:41 +0800

    Update Respawner.lua

--- pgt/Respawner.lua
+++ pgt/Respawner.lua
@@ -33,8 +33,10 @@ if game.PlaceId == 12413901502 then
 
             -- Click the Play button
             VM1Click(centerX, 325)
-            wait(10)
-            loadstring(game:HttpGet(('https://raw.githubusercontent.com/1337Coco/gbf/3b6ee1701c36d6fe8c817d92bdefb169feb502b9/pgt/Main.lua')))()
+            wait()
+            if playButton and not playButton.Visible then
+                loadstring(game:HttpGet(('https://raw.githubusercontent.com/1337Coco/gbf/3b6ee1701c36d6fe8c817d92bdefb169feb502b9/pgt/Main.lua')))()
+            end
         end --end if playButton visible
     end --end CheckAndClickPlayButton()
 
