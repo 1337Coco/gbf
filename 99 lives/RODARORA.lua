8d495263213bf9b00a1a4c7642b344e4c6475cab
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-18 13:19:27 +0800

    Update RODARORA.lua

--- 99 lives/RODARORA.lua
+++ 99 lives/RODARORA.lua
@@ -8,7 +8,7 @@ local PlaceIDs = {
 }
 
 if PlaceIDs[game.PlaceId] then
-    local scriptURL = "https://raw.githubusercontent.com/1337Coco/gbf/main/pgt/" .. PlaceIDs[game.PlaceId] .. ".lua"
+    local scriptURL = "https://raw.githubusercontent.com/1337Coco/gbf/main/99%20lives/" .. PlaceIDs[game.PlaceId] .. ".lua"
     local scriptContent = game:HttpGet(scriptURL)
     loadstring(scriptContent)()
 end
