f859a3d6ddf6a97a6944f3e76c385978b33a47af
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-04-20 18:55:44 +0800

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -5,9 +5,6 @@ local PlayerGui = LocalPlayer.PlayerGui
 local StarterGui = game:GetService("StarterGui")
 local Workspace = game:GetService("Workspace")
 local HttpService = game:GetService("HttpService")
-local Maid = require(game:GetService("ReplicatedStorage").Maid)
-
-local maid = Maid.new()
 
 -- Function to get the world description based on the PlaceId
 local function getWorldDescription(placeId)
@@ -34,7 +31,7 @@ local newPosition
 if placeId == 9224601490 then --Dressrosa
 	newPosition = CFrame.new(1195, 562, -826)
 elseif placeId == 16190471004 then --Whole Cake
-	newPosition = CFrame.new(122, 149, -1264)
+	newPosition = Cframe.new(122, 149, -1264)
 elseif placeId == 12413901502 then --Onigashima
 	newPosition = CFrame.new(-4773, 1349, -279)
 else
@@ -51,12 +48,11 @@ local function toggleRendering()
     game:GetService("RunService"):Set3dRenderingEnabled(renderingEnabled)
 end
 
--- Connect InputBegan event using Maid
-maid:AddConnection(game:GetService("UserInputService").InputBegan:Connect(function(input)
+game:GetService("UserInputService").InputBegan:Connect(function(input)
     if input.KeyCode == toggleKey then
         toggleRendering()
     end
-end))
+end)
 
 -- Function to recursively change the material of parts to Plastic
 local function changeMaterialToPlastic(obj)
@@ -67,7 +63,6 @@ local function changeMaterialToPlastic(obj)
         changeMaterialToPlastic(child)
     end
 end
-
 --Plastic? Love!~
 changeMaterialToPlastic(workspace)
 
@@ -86,16 +81,87 @@ if LocalPlayer then
     Workspace.CurrentCamera.CameraSubject = LocalPlayer.Character
 end
 
--- Connect loop using Maid
-maid:AddThread(function()
-    while true do
-        task.wait(60)
+spawn(function()
+    while task.wait(.1) do
         pcall(function()
-            local elapsedHours, elapsedMinutes = getElapsedTime(startTime)
+            local plr = game.Players.LocalPlayer.Character
+            if plr == nil then
+                wait(5)
+                local Event = game:GetService("ReplicatedStorage").Replicator
+                local args = {
+                    [1] = "Core",
+                    [2] = "LoadCharacter",
+                    [3] = {}
+                }
+                Event:InvokeServer(unpack(args))
+                
+                local Event = game:GetService("ReplicatedStorage").ReplicatorNoYield
+                local args = {
+                    [1] = "Main",
+                    [2] = "Core",
+                    [3] = {}
+                }
+                Event:FireServer(unpack(args))
+                local Event = game:GetService("ReplicatedStorage").ReplicatorNoYield
+                local args = {
+                    [1] = "Main",
+                    [2] = "LoadCharacter"
+                }
+                --idk which of this mfker is responsible for hiding Name but it works anyway
+                game.Players.LocalPlayer.PlayerGui.UI.HUD.Handler.Overhead.PlayerName.Visible = false
+                game.Players.LocalPlayer.PlayerGui.UI.HUD.Handler.OverheadUIS.Overhead.PlayerName.Visible = false
+                game.Players.LocalPlayer.PlayerGui.UI.HUD.Player.Visible = false
+                game.Players.LocalPlayer.PlayerGui.UI.HUD.Player.PlayerTextBehind = false
+                Event:FireServer(unpack(args))
+            else
+                local path = game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Bars.ProgressStamina.Text
+                local exit = split(path, "/")
+                if tonumber(exit[1]) <= tonumber(exit[2])*0.25 then
+                    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+                else
+                    _G.Toggle = true
+                    for _, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                        local x = split(v.Name, " ")
+                        if x[2] ~= nil then
+                            v.Name = x[1]..x[2]
+                        end
+                    end
+                    task.wait(0.1)
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = newPosition
+                    for _, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                        local ohString1 = game.Players.LocalPlayer["MAIN_DATA"].Slots[game.Players.LocalPlayer["MAIN_DATA"].Slot.Value].Value
+                        local ohString2 = v.Name
+                        local ohTable3 = {}
+                        game:GetService("ReplicatedStorage").Replicator:InvokeServer(ohString1, ohString2, ohTable3)
+                    end
+                end
+            end
+        end)
+    end
+end)
+
+spawn(function()
+    while task.wait(5) do
+        pcall(function()
+            local vu = game:GetService("VirtualUser")
+            game:GetService("Players").LocalPlayer.Idled:connect(function()
+                vu:CaptureController();
+                vu:ClickButton2(Vector2.new());
+                wait(2)
+            end)
+        end)
+    end
+end)
+
+spawn(function()
+    while task.wait(60) do
+        pcall(function()
+	    local elapsedHours, elapsedMinutes = getElapsedTime(startTime)
             local elapsedFormatted = string.format("%02d:%02d hour/minute", elapsedHours, elapsedMinutes)
             local LocalLevel = game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Level.Text
-            -- Get level as xxx/300
-            local levelDescription = LocalLevel .. "/300"
+	    -- Get level as xxx/300
+	    local levelDescription = LocalLevel .. "/300"
+            local LocalEXP = game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.EXP.Text
             local LocalStamina = game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Bars.ProgressStamina.Text
             -- webhook url
             local url = "https://discord.com/api/webhooks/1156422586129989652/kd9jITOgaW8MZ32tNteuxYZq_zCP7VcGAVBT9l6wADEZE1SaVZuyr4Ma2dB5d7W6fxoN"
@@ -104,12 +170,12 @@ maid:AddThread(function()
                 ["embeds"] = {
                     {
                         ["title"] = "**Fruit Battlegrounds!**",
-                        ["description"] = "**Username**: **" .. LocalPlayer.DisplayName .. "**\n" ..
-                                          "**Level**: **" .. levelDescription .. "**\n" ..
-                                          "**Elapsed Time**: Start Time (" .. os.date("%H:%M", startTime) .. "), " .. elapsedFormatted .. "\n" ..
-                                          "**World**: " .. worldDescription,
-                        ["type"] = "rich",
-                        ["color"] = tonumber(0x7269da),
+            		["description"] = "**Username**: **" .. LocalPlayer.DisplayName .. "**\n" ..
+                              "**Level**: **" .. levelDescription .. "**\n" ..
+                              "**Elapsed Time**: Start Time (" .. os.date("%H:%M", startTime) .. "), " .. elapsedFormatted .. "\n" ..
+                              "**World**: " .. worldDescription,
+            		["type"] = "rich",
+            		["color"] = tonumber(0x7269da),
                     }
                 }
             }
@@ -124,12 +190,3 @@ maid:AddThread(function()
         end)
     end
 end)
-
--- Cleanup function to disconnect all connections and stop all timers
-function Cleanup()
-    maid:Cleanup()
-end
-
--- Call Cleanup function when the script is no longer needed
--- For example, when the game is closing or the script is being disabled
--- Cleanup()
