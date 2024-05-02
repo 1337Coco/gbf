6b5287fc21d3588fb2bf7d94b9ae3c758c82c71b
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-05-02 10:45:18 +0800

    Update posCheck.lua

--- posCheck.lua
+++ posCheck.lua
@@ -6,9 +6,8 @@ local PlayerGui = LocalPlayer.PlayerGui
 local playButton = PlayerGui.UI.MainMenu.Buttons.Play
 
 if playButton then
-    -- Get the position of the playButton
-    local position = playButton.Position
-    print("Play button found at position:", position.X, position.Y)
+    -- Simulate a button click
+    playButton.MouseButton1Click:Fire()
 else
     print("Play button not found!")
 end
