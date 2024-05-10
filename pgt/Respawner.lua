f27643810e6320ad032b0568dd30057d29c7a7b8
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-05-11 00:23:30 +0800

    Update Respawner.lua

--- pgt/Respawner.lua
+++ pgt/Respawner.lua
@@ -13,7 +13,9 @@ local function VM1Click(X, Y)
         warn("VirtualInputManager not found.")
     end
 end
-
+-- Test
+VM1Click(150, 300)
+-- end Test
 -- Function to check if the Play button is visible and click it if it is
 local function CheckAndClickPlayButton()
     -- Find the Play button
