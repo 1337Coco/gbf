325149805a975a80ab98c16bc104b7fa0b2ebf2f
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-05-26 20:30:32 +0800

    Update pineapple.lua

--- pineapple.lua
+++ pineapple.lua
@@ -106,12 +106,14 @@ spawn(function()
                     _G.Toggle = true
 		if game.Workspace.Characters.NPCs:FindFirstChild("Marco") then
 			local Marco = game.Workspace.Characters.NPCs:WaitForChild("Marco")
+			--[[
 			if Marco:WaitForChild("Humanoid").Health >= 1 then
 			LocalPlayer.Character.HumanoidRootPart.CFrame = Marco:WaitForChild("HumanoidRootPart").CFrame * CFrame.new(0, 0, 3)
 			local ohString1 = "Core"
 			local ohString2 = "M1"
 			local ohTable3 = {}
 			game:GetService("ReplicatedStorage").Replicator:InvokeServer(ohString1, ohString2, ohTable3)
+			]]
 			end
                 elseif LocalPlayer.Character.HumanoidRootPart.CFrame ~= newPosition and not game.Workspace.Characters.NPCs:FindFirstChild("Marco") then
 			LocalPlayer.Character.HumanoidRootPart.CFrame = newPosition
