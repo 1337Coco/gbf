291768c57323b41f77ef61512bf437b7d5bbee6e
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-05-26 22:17:04 +0800

    Update pineapple.lua
    Prev. Version works but only punch when marco

--- pineapple.lua
+++ pineapple.lua
@@ -104,7 +104,14 @@ spawn(function()
                     LocalPlayer.Character:BreakJoints()
                 else
                     _G.Toggle = true
-                    if LocalPlayer.Character.HumanoidRootPart.CFrame ~= newPosition and not game.Workspace.Characters.NPCs:FindFirstChild("Marco") then
+		if Marco:WaitForChild("Humanoid").Health >= 1 then
+			LocalPlayer.Character.HumanoidRootPart.CFrame = Marco:WaitForChild("HumanoidRootPart").CFrame * CFrame.new(0, 0, 3)
+			local ohString1 = "Core"
+			local ohString2 = "M1"
+			local ohTable3 = {}
+			game:GetService("ReplicatedStorage").Replicator:InvokeServer(ohString1, ohString2, ohTable3)
+		end
+		elseif LocalPlayer.Character.HumanoidRootPart.CFrame ~= newPosition and not game.Workspace.Characters.NPCs:FindFirstChild("Marco") then
 			LocalPlayer.Character.HumanoidRootPart.CFrame = newPosition
                     end
                     for i,v in pairs(LocalPlayer:GetDescendants()) do
@@ -119,13 +126,6 @@ spawn(function()
                         end
 			if game.Workspace.Characters.NPCs:FindFirstChild("Marco") then
 				local Marco = game.Workspace.Characters.NPCs:WaitForChild("Marco")
-				if Marco:WaitForChild("Humanoid").Health >= 1 then
-					LocalPlayer.Character.HumanoidRootPart.CFrame = Marco:WaitForChild("HumanoidRootPart").CFrame * CFrame.new(0, 0, 3)
-					local ohString1 = "Core"
-					local ohString2 = "M1"
-					local ohTable3 = {}
-					game:GetService("ReplicatedStorage").Replicator:InvokeServer(ohString1, ohString2, ohTable3)
-				end
 			end
                     end
                 end
