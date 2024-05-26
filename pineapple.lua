7c3fac7a367fc32b4d04727b020f7833aefae375
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-05-26 22:21:48 +0800

    Update pineapple.lua

--- pineapple.lua
+++ pineapple.lua
@@ -58,6 +58,14 @@ if LocalPlayer then
     StarterGui:SetCoreGuiEnabled('PlayerList',false)
     Workspace.CurrentCamera.CameraSubject = LocalPlayer.Character
 end
+-- Marco checker
+spawn(function()
+	while task.wait() do
+		if game.Workspace.Characters.NPCs:FindFirstChild("Marco") then
+			local Marco = game.Workspace.Characters.NPCs:WaitForChild("Marco")
+		end
+	end
+end)
 
 -- Respawn, load character, tp to xyz coords, initialize skills, use skills. loop
 spawn(function()
@@ -104,14 +112,7 @@ spawn(function()
                     LocalPlayer.Character:BreakJoints()
                 else
                     _G.Toggle = true
-		if Marco:WaitForChild("Humanoid").Health >= 1 then
-			LocalPlayer.Character.HumanoidRootPart.CFrame = Marco:WaitForChild("HumanoidRootPart").CFrame * CFrame.new(0, 0, 3)
-			local ohString1 = "Core"
-			local ohString2 = "M1"
-			local ohTable3 = {}
-			game:GetService("ReplicatedStorage").Replicator:InvokeServer(ohString1, ohString2, ohTable3)
-		end
-		elseif LocalPlayer.Character.HumanoidRootPart.CFrame ~= newPosition and not game.Workspace.Characters.NPCs:FindFirstChild("Marco") then
+                    if LocalPlayer.Character.HumanoidRootPart.CFrame ~= newPosition and not game.Workspace.Characters.NPCs:FindFirstChild("Marco") then
 			LocalPlayer.Character.HumanoidRootPart.CFrame = newPosition
                     end
                     for i,v in pairs(LocalPlayer:GetDescendants()) do
@@ -121,12 +122,16 @@ spawn(function()
                                 ReplicatedStorage.Replicator:InvokeServer(GetFruit(),Attack)
                             else
                                 local Attack = v.Name:gsub(" ","")
+								local ohString1 = "Core"
+								local ohString2 = "M1"
+								local ohTable3 = {}
                                 ReplicatedStorage.Replicator:InvokeServer(GetFruit(),Attack)
                             end
                         end
-			if game.Workspace.Characters.NPCs:FindFirstChild("Marco") then
-				local Marco = game.Workspace.Characters.NPCs:WaitForChild("Marco")
-			end
+						--Marco
+						if Marco:WaitForChild("Humanoid").Health >= 1 then
+							LocalPlayer.Character.HumanoidRootPart.CFrame = Marco:WaitForChild("HumanoidRootPart").CFrame * CFrame.new(0, 0, 3)
+						end --Marco end
                     end
                 end
             end
