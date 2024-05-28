48fc8b7627f3e3e4dd011d0af3b80b4c89e18c4e
1337Coco <35539781+1337Coco@users.noreply.github.com>
2024-05-28 20:11:23 +0800

    Update nekcihc.lua

--- nekcihc.lua
+++ nekcihc.lua
@@ -129,15 +129,16 @@ spawn(function()
                             LocalPlayer.Character.HumanoidRootPart.CFrame = boss:WaitForChild("HumanoidRootPart").CFrame * CFrame.new(0, 0, 3)
                         end
                     end
-                    for i, v in pairs(LocalPlayer.Backpack:GetDescendants()) do
-						if v:GetAttribute('Name') then 
-							local Attack = v:GetAttribute('Name')
-							ReplicatedStorage.Replicator:InvokeServer(GetFruit(), Attack)
-						else
-							local Attack = v.Name:gsub(" ", "")
-							ReplicatedStorage.Replicator:InvokeServer(GetFruit(), Attack)
-						end
-					end
+                    for i, v in pairs(LocalPlayer:GetDescendants()) do
+                        if v.ClassName == 'Tool' then
+                            if v:GetAttribute('Name') then 
+                                ReplicatedStorage.Replicator:InvokeServer(CurrentData.Name, v, {})
+                            else
+                                local Attack = v.Name:gsub(" ", "")
+                                ReplicatedStorage.Replicator:InvokeServer(GetFruit(), Attack)
+                            end
+                        end
+                    end
                 end
             end
         end)
